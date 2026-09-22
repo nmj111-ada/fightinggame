cf33774328e23d0006cdea563e0f035f3485ae4a
nmj111-ada <a2585715671@163.com>
2026-09-22 12:59:17 +0800

    新增消耗道具动作和玩家背包 道具的使用逻辑已补全 对手掉落消耗品逻辑已补全 游戏升级完成 游戏登录页面已开启

--- src/App.java
+++ src/App.java
@@ -6,9 +6,9 @@ public class App {
     //启动类
     //启动，登录，注册页面
     public static void main(String[] args) {
-        /*Login login = new Login();
+        Login login = new Login();
         login.start();
-        */
+
 
         FightingGame fg = new FightingGame();
         fg.gameStart("username");
